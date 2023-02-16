d72e0d91d6e66c0ca147d92264561d0d060e0753
Klim Huynh <klimhuynh@gmail.com>
2023-02-16 17:34:48 +1100

    Add endpoints

---
+++ src/test/java/com/mnfll/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnfll.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
